c3a024b2d551e1b9314e7d812b55f589401e46e6
joanamorais13 <joanamorais13@gmail.com>
2025-01-23 15:03:05 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather App</h1>
+      </header>
+    </div>
+  );
+}
+
+
