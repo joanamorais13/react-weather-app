03b2d12775a11181c3eb404f1e34a702ce99e660
joanamorais13 <joanamorais13@gmail.com>
2025-01-23 15:52:12 +0000

    updated css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import './index.css';
 
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
-    </div>
+    <div></div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,20 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Weather from './Weather';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-        <App />
-
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather App</h1>
+      </header>
+    </div>
         <Weather />
-
+        <footer>
+          <p className="github-note">
+      This project is open sourced on 
+      <strong> 
+        <a href="https://github.com/joanamorais13/react-weather-app"  target="_blank"
+  rel="noopener noreferrer"
+          > GitHub <i className="fa-brands fa-github"></i></a
+      ></strong>
+    </p>
+        </footer>
+        
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
